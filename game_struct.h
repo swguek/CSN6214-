3ba24d1c2c20d90349500c876d98dec6db5e6a9c
swguek <guekshanwei@gmail.com>
2026-02-04 21:51:25 +0800

    Add GameState structure for game management

---
+++ game_struct.h
@@ -0,0 +1,27 @@
+#ifndef GAME_STRUCT_H
+#define GAME_STRUCT_H
+
+#include <pthread.h>
+#include <semaphore.h>
+#include "game_logic.h" 
+
+#define LOG_QUEUE_SIZE 20
+#define LOG_MSG_LEN 256
+
+typedef struct {
+    int player_count;
+    int current_turn;
+    int game_active;
+    int chests[10];
+    int scores[5]; 
+    int turn_complete;      
+    int active_players[MAX_PLAYERS];
+    char log_queue[LOG_QUEUE_SIZE][LOG_MSG_LEN];
+    int log_head, log_tail, log_count;
+    pthread_mutex_t log_mutex;
+    sem_t log_sem;
+    GameLogic logic; 
+    
+} GameState;
+
+#endif
