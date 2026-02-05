8543d434c36a4d4702199a5cd69e617bc42411fa
Ghostgmyt <167853701+Ghostgmyt@users.noreply.github.com>
2026-02-05 19:37:09 +0800

    Update game_logic.h

--- game_logic.h
+++ game_logic.h
@@ -6,26 +6,24 @@
 
 // Tile types
 #define TILE_NONE   0
-#define TILE_BOOST  1
-#define TILE_TRAP   2
-#define TILE_BACK   3
+#define TILE_BOOST  1   // go forward extra
+#define TILE_TRAP   2   // skip next turn
+#define TILE_BACK   3   // go backward
 
 typedef struct {
     int type;
-    int value;
+    int value;   // used for BOOST/BACK, ignored for TRAP/NONE
 } Tile;
 
 typedef struct {
     int pos[MAX_PLAYERS];
     int skip[MAX_PLAYERS];
-    int winner;
-    Tile board[BOARD_SIZE + 1];
+    int winner;                 // -1 if none, else player index
+    Tile board[BOARD_SIZE + 1]; // 0..50
 } GameLogic;
 
-// initialize board and players
 void init_game(GameLogic *g, int player_count);
 
-// apply one player turn
 void play_turn(GameLogic *g, int player_id, int dice,
                char *msg, int msg_size);
 
